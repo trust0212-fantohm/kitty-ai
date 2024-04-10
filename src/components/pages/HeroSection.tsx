d900e930dcb04a71d70960b5ed9df94cc3bfc04a
trust0212-fantohm <zaza.liveedu@gmail.com>
2024-04-10 05:10:37 -0700

    increased kitty hero image

--- src/components/pages/HeroSection.tsx
+++ src/components/pages/HeroSection.tsx
@@ -16,15 +16,17 @@ const HeroSection: React.FC<Props> = ({ className }) => {
         className,
       )}
     >
-      <div className='container relative flex !max-w-[1536px] items-center justify-around gap-12 pt-20 md:flex-col xs:gap-4'>
-        <Image
-          src={KittyHeroImage}
-          alt='Kitty Hero'
-          width={896}
-          height={640}
-          className='lg:w-[576px] md:w-[480px] xs:w-80'
-        />
-        <PresaleForm className='md:z-10' />
+      <div className='container relative flex !max-w-[1536px] items-center justify-around gap-12 overflow-hidden pt-20 md:flex-col xs:gap-4'>
+        <div className='sm:-mx-20'>
+          <Image
+            src={KittyHeroImage}
+            alt='Kitty Hero'
+            width={896}
+            height={640}
+            className='lg:w-[576px] md:w-[480px]'
+          />
+        </div>
+        <PresaleForm className='md:z-10 md:-mt-40' />
       </div>
       <div className='pointer-events-none absolute -bottom-px h-40 w-full bg-gradient-to-b from-transparent via-gray-900/70 to-gray-900' />
     </section>
