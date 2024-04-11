b215fd94e6aadfa6a1d2e9079ca27fb102f8c99c
trust0212-fantohm <zaza.liveedu@gmail.com>
2024-04-11 07:51:34 -0700

    updated hero image size and position

--- src/components/pages/HeroSection.tsx
+++ src/components/pages/HeroSection.tsx
@@ -16,17 +16,17 @@ const HeroSection: React.FC<Props> = ({ className }) => {
         className,
       )}
     >
-      <div className='container relative flex !max-w-[1536px] items-center justify-around overflow-hidden pt-20 md:flex-col xs:gap-4 h-screen md:h-auto'>
+      <div className='container relative flex h-screen !max-w-[1536px] items-center justify-end pt-20 lg:h-auto lg:flex-col xs:gap-4'>
         <div className='mt-auto'>
           <Image
             src={KittyHeroImage}
             alt='Kitty Hero'
             width={896}
             height={640}
-            className='lg:w-[576px] md:w-[480px]'
+            className='absolute bottom-0 right-1/3 w-[58vw] lg:w-[576px] lg:relative lg:right-[inherit] md:w-[480px]'
           />
         </div>
-        <PresaleForm className='md:z-10 md:-mt-10' />
+        <PresaleForm className='lg:z-10 md:-mt-28 lg:-mt-40' />
       </div>
       <div className='pointer-events-none absolute -bottom-px h-40 w-full bg-gradient-to-b from-transparent via-gray-900/70 to-gray-900' />
     </section>
