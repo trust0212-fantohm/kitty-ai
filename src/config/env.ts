81845abbd82cdfd29165228dbc81c9c6acc9eb1f
trust0212-fantohm <zaza.liveedu@gmail.com>
2024-04-11 20:11:03 -0700

    updated env

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,4 +5,4 @@ export const SOL_PROGRAM = process.env.NEXT_PUBLIC_SOL_PROGRAM as string
 export const SOL_ACCOUNT = process.env.NEXT_PUBLIC_SOL_ACCOUNT as string
 export const SOL_PRICE_FEED = process.env.NEXT_PUBLIC_SOL_PRICE_FEED as string
 export const SOL_PAYMENT_WALLET = process.env.NEXT_PUBLIC_SOL_PAYMENT_WALLET as string
-export const DEV_MODE = process.env.NEXT_PUBLIC_DEV_MODE == 'true'
+export const DEV_MODE = process.env.NEXT_PUBLIC_DEV_MODE === 'true'
